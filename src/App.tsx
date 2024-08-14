e330e50ec250aab8fd0eee0b389c65f00f55cc78
Sofia Bargues <barguessofia@gmail.com>
2024-08-14 23:36:14 +0100

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
 function App() {
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <>
       <h1>Password Strength Checker</h1>
       <div className="card">
-        <input></input>
-        <div className="h-3 w-20 text-red-600">se</div>
+        <input onChange={handleChange} className="border border-1"></input>
+        <div className="h-3 w-44 bg-slate-300">
+          <div
+            className="h-3 bg-lime-500"
+            style={{
+              width: "90%",
+            }}
+          ></div>
+        </div>
         <p>Strength of your password (out of 10) is IO</p>
       </div>
     </>
